40108a942b60bf3dac99b752c476111840b86612
Akilan <akilankamal@gmail.com>
2020-08-17 00:54:05 +0530

    deintegrating pods add .gitignore

--- Pods/Target Support Files/Pods-CalciLibrary-CalciLibraryTests/Pods-CalciLibrary-CalciLibraryTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CalciLibrary_CalciLibraryTests : NSObject
-@end
-@implementation PodsDummy_Pods_CalciLibrary_CalciLibraryTests
-@end
--- Pods/Target Support Files/Pods-CalciLibrary/Pods-CalciLibrary-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CalciLibrary : NSObject
-@end
-@implementation PodsDummy_Pods_CalciLibrary
-@end
