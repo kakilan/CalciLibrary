60e90f247439712e12d00cef3a0cddc3efc51672
Akilan <akilankamal@gmail.com>
2020-08-20 16:05:32 +0530

    Identifying Online and Offline operation as independent entities

--- CalciLibrary/Protocols/Calculator.swift
+++ CalciLibrary/Protocols/Calculator.swift
@@ -6,16 +6,4 @@
 //  Copyright © 2020 leoVegas. All rights reserved.
 //
 
-public protocol Calculator {
-    
-    func add(operand1: String, and operand2: String) -> String
-    func subtract(operand1: String, and operand2: String) -> String
-    func multiply(operand1: String, with operand2: String) -> String
-    func divide(operand1: String, by operand2:String) -> String
-    func sineOf(degrees: String) -> String
-    func cosineOf(degrees: String) -> String
-    func bitValueOf(coin: String, completion: @escaping(_ result: String) -> Void)
-    func addressOf(latitude: String, longitude: String, completion: @escaping(_ result: String) -> Void)
-    
-    func getAvailableOperations(completion: @escaping(_ supportedOperations: Array<String>) -> Void)
-}
+public protocol Calculator: OfflineOperations, OnlineOperations, RemoteConfigurable { }
